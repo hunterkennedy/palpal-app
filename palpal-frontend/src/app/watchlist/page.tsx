59259159939224b3f63941c4327e4faef2ad6e96
Hunter <hunterkennedy@users.noreply.github.com>
2026-03-24 15:40:44 -0700

    cache

--- palpal-frontend/src/app/watchlist/page.tsx
+++ palpal-frontend/src/app/watchlist/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Navbar from '@/components/Navbar';
 import WatchlistPage from '@/components/WatchlistPage';
 import { getEpisodes, getPodcasts } from '@/lib/conductor';
